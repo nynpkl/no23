561739f9f3a639a543229226a815446890d43043
nynpkl <nynpkl@gmail.com>
2026-05-04 15:48:56 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>No23 Basketbol Maç Arşivi</h1>
+
+      <div style={{ marginTop: 20 }}>
+        <h3>No23 vs Fatih Belediye</h3>
+        <a href="https://youtube.com" target="_blank">
+          Maçı İzle
+        </a>
+        <br />
+        <a href="https://docs.google.com" target="_blank">
+          İstatistik
+        </a>
+      </div>
+    </div>
+  );
+}
