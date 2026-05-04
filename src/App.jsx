abb232ebdefa82fb18006a8990b1fe3eb268c4e6
nynpkl <nynpkl@gmail.com>
2026-05-04 17:37:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
 import { useState } from "react";
-
-const matches = [
-  {
-    id: 1,
-    date: "2026-05-01",
-    age: "U10",
-    league: "TBF",
-    result: "Mağlubiyet",
-    opponent: "Fatih Belediyesi",
-    score: "31-33",
-    video: "https://youtube.com",
-    stats: "https://docs.google.com"
-  },
-  {
-    id: 2,
-    date: "2026-04-26",
-    age: "U9",
-    league: "Unibest",
-    result: "Galibiyet",
-    opponent: "Bahçeşehir İhtisas B",
-    score: "14-6",
-    video: "https://youtube.com",
-    stats: "https://docs.google.com"
-  }
-];
+import matches from "./matches";
 
 export default function App() {
   const [ageFilter, setAgeFilter] = useState("All");
