b970eba5ac70c909a760213ec9bbec3c399f0c56
nynpkl <nynpkl@gmail.com>
2026-05-04 21:25:13 +0300

    Update matches.js

--- src/matches.js
+++ src/matches.js
@@ -9,7 +9,7 @@ id: 1,
     score: "40-32",
     video: "https://youtu.be/wMCPAHz5180",
     highlights: "https://youtu.be/lzhVcqnnmHg",
-    stats: "ehttps://easystats-bball.s3.amazonaws.com/0BE86B7D-1EBC-43BD-9F40-30D2025BCCC5.html"
+    stats: "https://easystats-bball.s3.amazonaws.com/0BE86B7D-1EBC-43BD-9F40-30D2025BCCC5.html"
 ];
 
 export default matches;
