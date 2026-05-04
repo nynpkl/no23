a5c2c3219d826e5b3d09f52a982b25aa3b7eb7e0
nynpkl <nynpkl@gmail.com>
2026-05-04 17:36:48 +0300

    Create matches.js

---
+++ src/matches.js
@@ -0,0 +1,26 @@
+const matches = [
+  {
+    id: 1,
+    date: "2026-05-01",
+    age: "U10",
+    league: "TBF",
+    result: "Mağlubiyet",
+    opponent: "Fatih Belediyesi",
+    score: "31-33",
+    video: "https://youtube.com",
+    stats: "https://docs.google.com"
+  },
+  {
+    id: 2,
+    date: "2026-04-26",
+    age: "U9",
+    league: "Unibest",
+    result: "Galibiyet",
+    opponent: "Bahçeşehir İhtisas B",
+    score: "14-6",
+    video: "https://youtube.com",
+    stats: "https://docs.google.com"
+  }
+];
+
+export default matches;
