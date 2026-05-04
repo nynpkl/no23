9637c8756b2b45bee4a8599bf665abbc060793c5
nynpkl <nynpkl@gmail.com>
2026-05-04 21:23:48 +0300

    Update matches.js

--- src/matches.js
+++ src/matches.js
@@ -1,6 +1,5 @@
 const matches = [
-{
-    id: 1,
+id: 1,
     season: "2025-2026",
     date: "2026-01-03",
     age: "U9",
@@ -11,7 +10,6 @@ const matches = [
     video: “https://youtu.be/wMCPAHz5180“,
     highlights: “https://youtu.be/lzhVcqnnmHg“,
     stats: “ehttps://easystats-bball.s3.amazonaws.com/0BE86B7D-1EBC-43BD-9F40-30D2025BCCC5.html“
-  }
 ];
 
 export default matches;
