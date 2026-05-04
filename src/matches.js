cfe040f606ae75ad6a31d09547eff67f9112dbca
nynpkl <nynpkl@gmail.com>
2026-05-04 21:28:21 +0300

    Update matches.js

--- src/matches.js
+++ src/matches.js
@@ -11,7 +11,20 @@ const matches = [
     video: "https://youtu.be/wMCPAHz5180",
     highlights: "https://youtu.be/lzhVcqnnmHg",
     stats: "https://easystats-bball.s3.amazonaws.com/0BE86B7D-1EBC-43BD-9F40-30D2025BCCC5.html"
-    }
+    },
+    {
+    id: 2,
+    season: "2025-2026",
+    date: "2026-02-22",
+    age: "U9",
+    league: "Gelişim Ligi",
+    result: "Galibiyet",
+    opponent: "Sarıyer Yıldızları",
+    score: "51-04",
+    video: "https://youtu.be/8kvtyW0xYEY",
+    highlights: "https://youtu.be/Li76M1V5Otg",
+    stats: "https://easystats-bball.s3.amazonaws.com/D37D6467-1FBD-4B47-92CC-8E4C51B60F75.html"
+  }
 ];
 
 export default matches;
